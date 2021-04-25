09e365cfc7e6dcbeee81078a01cbdcb53c84d089
Ngizeh Mwas <edngize@gmail.com>
2021-04-25 21:44:58 +0300

    Added Inertia support, tailwind and Vue on the front end

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="bg-red-500">
+		<h2>Hello Photo Resize</h2>
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Home',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
